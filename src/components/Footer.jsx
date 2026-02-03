4356845ce5cab0a6f75c97e9d019cc39345a51a4
Joao-Aquino25 <joaoovcarvalhoo@gmail.com>
2026-02-03 14:38:24 -0300

    Complete iPhone 17 project with full layout and React components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,71 @@
 function Footer() {
+  const sections = [
+    {
+      title: "Comprar e Saber Mais",
+      links: [
+        "iPhone 17 Pro",
+        "iPhone 17 Pro Max",
+        "Comparar modelos",
+        "Acessórios",
+      ],
+    },
+    {
+      title: "Especificações",
+      links: ["Características técnicas", "Câmera", "Bateria", "Display"],
+    },
+    {
+      title: "Suporte",
+      links: ["Suporte ao iPhone", "AppleCare+", "iOS 26", "Contato"],
+    },
+    {
+      title: "Apple",
+      links: ["Sobre a Apple", "Newsroom", "Privacidade", "Carreiras"],
+    },
+  ];
+
+  const buttonLinks = ["Politica de Privacidade", "Termos de Uso", "Vendas"];
+
   return (
-    <div>
-      <h2>Footer</h2>
-    </div>
+    <footer className="bg-gray-900 border-t border-gray-800">
+      <div className="max-w-7xl mx-auto px-6 py-12">
+        <div className="grid md:grid-cols-4 gap-8 mb-8">
+          {sections.map((section, index) => (
+            <div key={index}>
+              <h4 className="font-semibold mb-4">{section.title}</h4>
+              <ul className="space-y-2 text-sm text-gray-400">
+                {section.links.map((link, linkIndex) => (
+                  <li key={linkIndex}>
+                    <a href="#" className="hover:text-white cursor-pointer">
+                      {link}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="border-t border-gray-800 pt-6">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-4">
+          <p className="text-md text-gray-400">
+            Copyright © 2026 Apple Inc. Todos os direitos reservados.
+          </p>
+          <div className="flex gap-6 text-md text-gray-400">
+            {buttonLinks.map((buttonLink, index) => (
+              <a className="hover:text-white" href="#" key={index}>
+                {buttonLink}
+              </a>
+            ))}
+          </div>
+        </div>
+        <p className="text-xs text-gray-300 mt-6">
+          Este projeto foi desenvolvido exclusivamente para fins educacionais.
+          Todas as marcas e imagens utilizadas pertencem aos seus respectivos
+          proprietários.
+        </p>
+      </div>
+    </footer>
   );
 }
 
